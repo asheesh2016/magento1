c12c7df5253e12fd542436c2b3afb01c8590b4d9
asheesh2016 <asheesh@amplifymedi.in>
2018-10-23 01:24:02 -0700

    initial commsit

---
+++ app/etc/env.php
@@ -0,0 +1,49 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'npxetbhmyimj0rtj'
+    ],
+    'crypt' => [
+        'key' => '0ed08f798532d63ecb5e3e7265d5321e'
+    ],
+    'db' => [
+        'table_prefix' => 'mg_',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'i3906625_mg2',
+                'username' => 'i3906625_mg2',
+                'password' => 'W.Pi2ZmHWPfqQWCUZpQ03',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'install' => [
+        'date' => 'Tue, 23 Oct 2018 07:38:52 +0000'
+    ]
+];
